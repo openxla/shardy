c01844ce5d5097fae4ea529c33c8d7203435d6b9
shardy authors <google-ml-automation@google.com>
2025-07-16 12:33:28 -0700

    Migrate away from ArrayRef(std::nullopt_t)
    The upstream LLVM has deprecated ArrayRef(std::nullopt_t).  This CL migrates away from that.
    
    PiperOrigin-RevId: 783855159

--- shardy/dialect/sdy/transforms/export/export_pipeline.cc
+++ shardy/dialect/sdy/transforms/export/export_pipeline.cc
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include <optional>
 #include <string>
 
 #include "mlir/Dialect/Func/IR/FuncOps.h"
@@ -33,7 +32,7 @@ namespace {
 void addCanonicalizerPass(OpPassManager& pm,
                           ArrayRef<std::string> enabledPatterns) {
   pm.addPass(createCanonicalizerPass(GreedyRewriteConfig(),
-                                     /*disabledPatterns=*/std::nullopt,
+                                     /*disabledPatterns=*/{},
                                      /*enabledPatterns=*/enabledPatterns));
 }
 
