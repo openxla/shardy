e90804d9b605c61ac68c27d6b395799d8c7d79fd
Peter Buchlovsky <petebu@google.com>
2026-04-29 09:17:50 -0700

    Add UniquifyAndMergeReturnsPass to merge return copies into existing fragments.
    Instead of creating intermediate inferred fragments that need subsequent
    cleanup passes, this new pass directly adds extra results/operands to
    existing fragments, producing the same result as running uniquify +
    merge-inferred-fragments (producer/consumer + sideways) in a single step.
    
    The export pipeline is updated to use this new pass. The existing
    transfer-based UniquifyFunctionInputsOutputsPass is unaffected.
    
    PiperOrigin-RevId: 907624587

--- shardy/dialect/mpmd/transforms/export/export_pipeline.cc
+++ shardy/dialect/mpmd/transforms/export/export_pipeline.cc
@@ -84,12 +84,7 @@ void addExportPipeline(OpPassManager& pm, const ExportOptions& options) {
   // Must be applied after the last -mpmd-fragment-dedup, as it may add
   // duplicated fragment results and after -canonicalize, as it may add
   // identity fragments, which would be canonicalized away.
-  pm.addNestedPass<FuncOp>(createUniquifyFunctionInputsOutputsPass());
-
-  // The fragments created by the pass above maybe slowdown compilation (more
-  // fragments to compile) and may cause performance regressions. Thus, we merge
-  // them with other fragments.
-  pm.addNestedPass<FuncOp>(createMergeInferredFragmentsPass());
+  pm.addNestedPass<FuncOp>(createUniquifyAndMergeReturnsPass());
 
   // Mark each fragment with the inputs and outputs which are offloaded to host
   // memory.
