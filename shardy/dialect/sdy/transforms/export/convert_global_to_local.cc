fd782a4ae3e3687553a8ad859405a90001681f5c
Bixia Zheng <bixia@google.com>
2026-09-17 07:43:38 -0700

    Fix the handling of stablehlo.scatter in convert-global-to-local.
    Previously, we create isLeader tensor as a 1D tensor and then broadcast it to the local input shape using broadcast with dims=[0]. This doesn't work when local input is a rank 0 tensor. Fix to make isLeader a scalar.
    
    PiperOrigin-RevId: 983189942

--- shardy/dialect/sdy/transforms/export/convert_global_to_local.cc
+++ shardy/dialect/sdy/transforms/export/convert_global_to_local.cc
@@ -2531,11 +2531,9 @@ class StablehloScatterOpPattern
               tableConst, {partitionId}, rewriter.getDenseI64ArrayAttr({1}))
               .getResult();
 
-      isLeader = stablehlo::BroadcastInDimOp::create(
-          rewriter, loc,
-          RankedTensorType::get(localInputType.getShape(),
-                                rewriter.getI1Type()),
-          isLeader, rewriter.getDenseI64ArrayAttr({0}));
+      isLeader = stablehlo::ReshapeOp::create(
+          rewriter, loc, RankedTensorType::get({}, rewriter.getI1Type()),
+          isLeader);
       // Broadcast the scalar identity to the shape of the current local input.
       Value broadcastIdentity = stablehlo::BroadcastInDimOp::create(
           rewriter, loc, localInputs.front().getType(), identity,
